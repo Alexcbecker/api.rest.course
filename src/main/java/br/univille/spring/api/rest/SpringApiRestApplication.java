de90d3f9c48ea1786d63b1551c67cdeb0ec9fcad
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-07-16 20:28:10 -0300

    first commit

---
+++ src/main/java/br/univille/spring/api/rest/SpringApiRestApplication.java
@@ -0,0 +1,12 @@
+package br.univille.spring.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+}
---
+++ src/test/java/br/univille/spring/api/rest/SpringApiRestApplicationTests.java
@@ -0,0 +1,16 @@
+package br.univille.spring.api.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringApiRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
